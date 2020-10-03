232f8ff21a412346f8a0999a43593beea5ff7798
steven marin <stevenmariin4@gmail.com>
2020-10-03 12:31:13 -0500

    publicacion version 0.3.0

--- meang-frontend/src/app/@Public/pages/public.module.ts
+++ meang-frontend/src/app/@Public/pages/public.module.ts
@@ -5,7 +5,7 @@ import { PublicRoutingModule } from './public-routing.module';
 import { PublicComponent } from './public.component';
 import { HeaderComponent } from '@shop-core/components/header/header.component';
 import { NavbarComponent } from '@shop-core/components/navbar/navbar.component';
-import { FooterComponent } from '../core/components/footer/footer.component';
+import { FooterComponent } from '@shop-core/components/footer/footer.component';
 
 @NgModule({
   declarations: [PublicComponent, HeaderComponent, NavbarComponent, FooterComponent],
