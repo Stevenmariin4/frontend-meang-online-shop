477ece4db39866e487adc259e53a9a36d4686c86
steven marin <stevenmariin4@gmail.com>
2020-10-03 12:00:06 -0500

    plantilla base de la tienda

--- meang-frontend/src/app/@Admin/pages/admin.module.ts
+++ meang-frontend/src/app/@Admin/pages/admin.module.ts
@@ -1,14 +1,19 @@
-import { TitleComponent } from './../core/components/title/title.component';
+import { TitleComponent } from '@admin-core/components/title/title.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
-import { HeaderComponent } from '../core/components/header/header.component';
-import { SidebarComponent } from '../core/components/sidebar/sidebar.component';
+import { HeaderComponent } from '@admin-core/components/header/header.component';
+import { SidebarComponent } from '@admin-core/components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [AdminComponent, HeaderComponent, TitleComponent, SidebarComponent],
+  declarations: [
+    AdminComponent,
+    HeaderComponent,
+    TitleComponent,
+    SidebarComponent,
+  ],
   imports: [CommonModule, AdminRoutingModule],
 })
 export class AdminModule {}
